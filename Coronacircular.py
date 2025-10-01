e244f4c579d0bffb94a965212b1114aa3b7b734c
Harss-Lluis-C-D-C <229790891+Harss12-p@users.noreply.github.com>
2025-10-01 16:27:56 -0500

    Update Coronacircular.py

--- Coronacircular.py
+++ Coronacircular.py
@@ -1,8 +1,15 @@
+print("")
+print("""  ▀      ▀▀▀▀▀▀▀▀
+           ▀      ▀
+           ▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+                  ▄     ▄
+           ▄▄▄▄▄▄▄▄     ▄""")
+print("")
 print("Corona circular")
-radio_ma = float(input("ingrese radio mayor: "))
-radio_me = float(input("ingrese radio menor: "))
-area = 3.14*(radio_ma**2 - radio_me**2)
-perimetro = 2*3.14*radio_ma + 2*3.14*radio_me
-print("resultado")
-print("Area:", area.__around__(2))
-print("Perimetro:", perimetro.__around__(2))
+radio_ma = float(input("Ingrese radio mayor: "))
+radio_me = float(input("Ingrese radio menor: "))
+area = 3.14 * (radio_ma**2 - radio_me**2)
+perimetro = 2 * 3.14 * radio_ma + 2 * 3.14 * radio_me
+print("Resultado")
+print(f"Area: {area:.2f}")
+print(f"Perimetro: {perimetro:.2f}")
