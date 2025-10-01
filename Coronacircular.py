9e6492bf818f83aa32f2388cb070ceb8999c8261
RiyanPC <locochon56303@gmail.com>
2025-10-01 15:30:19 -0500

    sistema corona con perimetro

--- Coronacircular.py
+++ Coronacircular.py
@@ -2,5 +2,7 @@ print("Corona circular")
 radio_ma = float(input("ingrese radio mayor: "))
 radio_me = float(input("ingrese radio menor: "))
 area = 3.14*(radio_ma**2 - radio_me**2)
+perimetro = 2*3.14*radio_ma + 2*3.14*radio_me
 print("resultado")
-print("Area:", area)
+print("Area:", area.__around__(2))
+print("Perimetro:", perimetro.__around__(2))
